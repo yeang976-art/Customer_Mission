f634e7c86ea466481ee5f9fde0ebeb2d8597acd3
yeang976-art <yeang976@gmail.com>
2026-06-16 19:01:53 +0900

    유지보수성을 위해 컬렉션으로 상품 등록 모듈화

--- Customer_System_Pack/src/Customer_System/Main.java
+++ Customer_System_Pack/src/Customer_System/Main.java
@@ -1,10 +1,18 @@
 package Customer_System;
 
+import java.util.*;
+
 public class Main {
     static void main(String[] args) {
-        Product p1 = new Product("Galaxy S25", 1200000L, "최신 안드로이드 스마트폰", 5);
-        Product p2 = new Product("iPhone 16", 1350000L, "Apple의 최신 스마트폰", 5);
-        Product p3 = new Product("MacBook Pro", 2400000L, "M3 칩셋이 탑재된 노트북", 5);
-        Product p4 = new Product("AirPods Pro", 350000L, "노이즈 캔슬링 무선 이어폰", 5);
+        List<Product> products = createProducts();
+    }
+
+    private static List<Product> createProducts() {
+        return new ArrayList<>(List.of(
+                new Product("Galaxy S25", 1200000L, "최신 안드로이드 스마트폰", 5),
+                new Product("iPhone 16", 1350000L, "Apple의 최신 스마트폰", 5),
+                new Product("MacBook Pro", 2400000L, "M3 칩셋이 탑재된 노트북", 5),
+                new Product("AirPods Pro", 350000L, "노이즈 캔슬링 무선 이어폰", 5)
+        ));
     }
 }
